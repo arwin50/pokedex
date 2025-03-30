741cfa82e4dae5237b872902b00726e7c854c178
powtzulist <delasanarwin@gmail.com>
2025-03-30 22:15:03 +0800

    speed insights fix 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { PokemonCardList } from "./components/Pokemon/PokemonCardView/PokemonCardList";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
     <main className="flex justify-center min-h-screen py-6 bg-gradient-to-b from-gray-50 to-gray-100 ">
       <PokemonCardList />
-      <SpeedInsights />
+     
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,12 @@ import NotFound from "./NotFound";
 import "./index.css";
 import App from "./App";
 import { PokemonPage } from "./components/Pokemon/PokemonPage/PokemonPage";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <SpeedInsights />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="pokemon/:id" element={<PokemonPage />} />
@@ -19,6 +20,5 @@ createRoot(document.getElementById("root")!).render(
         />
       </Routes>
     </BrowserRouter>
-   
   </StrictMode>
 );
